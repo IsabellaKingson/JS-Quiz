9749250907de3b018b93e662218cab74f5e332b4
IsabellaKingson <isabellasophiaking@gmail.com>
2023-07-08 14:38:27 -0500

    Added an array of objects containing the quiz questions.

---
+++ assets/js/script.js
@@ -0,0 +1,122 @@
+const questions = [
+  {
+    Q: "What are the primitive data types?",
+    A: [
+      {
+        text: "String, number, boolean, undefined, null, bigint, and symbol.",
+        isCorrect: true,
+      },
+      { text: "String, number, and boolean.", isCorrect: false },
+      { text: "Objects, functions, and arrays.", isCorrect: false },
+      { text: "Characters, numbers, null, and undefinifed.", isCorrect: false },
+    ],
+  },
+  {
+    Q: "In which tag do we attach a JavaScript file to an HTML Document?",
+    A: [
+      {
+        text: "<script>",
+        isCorrect: true,
+      },
+      { text: "<href>", isCorrect: false },
+      { text: "<link>", isCorrect: false },
+      { text: "<a>", isCorrect: false },
+    ],
+  },
+  {
+    Q: "Which answer properly declares and initialized a variable?",
+    A: [
+      {
+        text: "const variable = value;",
+        isCorrect: true,
+      },
+      { text: "variable const = value;", isCorrect: false },
+      { text: "let value = variable;", isCorrect: false },
+      { text: "var is value", isCorrect: false },
+    ],
+  },
+  {
+    Q: "Objects are made up of which type of pairs?",
+    A: [
+      {
+        text: "Key-value",
+        isCorrect: true,
+      },
+      { text: "Index-item", isCorrect: false },
+      { text: "Half-whole", isCorrect: false },
+      { text: "This-that", isCorrect: false },
+    ],
+  },
+  {
+    Q: "What is the scope of a variable declared within a function?",
+    A: [
+      {
+        text: "Local scope",
+        isCorrect: true,
+      },
+      { text: "Global scope", isCorrect: false },
+      { text: "Magnified", isCorrect: false },
+      { text: "Broad scope", isCorrect: false },
+    ],
+  },
+  {
+    Q: "What is used to iterate over a collection of data?",
+    A: [
+      {
+        text: "A for loop",
+        isCorrect: true,
+      },
+      { text: "A function expression", isCorrect: false },
+      { text: "An if else statement", isCorrect: false },
+      { text: "An each-every statement", isCorrect: false },
+    ],
+  },
+  {
+    Q: "Which statement says that 'a' is greater than or equal to 'b'?",
+    A: [
+      {
+        text: "a >= b",
+        isCorrect: true,
+      },
+      { text: "a === b", isCorrect: false },
+      { text: "a > b && a = b", isCorrect: false },
+      { text: "a > b", isCorrect: false },
+    ],
+  },
+  {
+    Q: "What is a reusable block of code that performs a specific task?",
+    A: [
+      {
+        text: "A function",
+        isCorrect: true,
+      },
+      { text: "A bug", isCorrect: false },
+      { text: "A string", isCorrect: false },
+      { text: "A variable", isCorrect: false },
+    ],
+  },
+  {
+    Q: "What method is used to print to the console?",
+    A: [
+      {
+        text: "console.log();",
+        isCorrect: true,
+      },
+      { text: "console.print();", isCorrect: false },
+      { text: "console.write();", isCorrect: false },
+      { text: "console.please();", isCorrect: false },
+    ],
+  },
+  {
+    Q: "Which answer is a conditional statement?",
+    A: [
+      {
+        text: "An if else statement",
+        isCorrect: true,
+      },
+      { text: "A for loop", isCorrect: false },
+      { text: "The string, 'conditional'", isCorrect: false },
+      { text: "An object", isCorrect: false },
+    ],
+  },
+];
